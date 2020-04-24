22c08aabba2b66708490458e764e957666241a66
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2020-04-25 00:41:00 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='django-admin-action-buttons',
     description='Django admin tweak that replaces list action dropdown with '
                 'buttons',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/frnhr/django-admin-action-buttons',
     author='Fran Hrzenjak',
     author_email='fran@changeset.hr',
