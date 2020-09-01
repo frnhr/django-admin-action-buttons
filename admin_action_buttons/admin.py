6c2a8c0abab735dca57dbedf9f64398f0eba0920
Fran Hrzenjak <fran.hrzenjak@gmail.com>
2020-09-01 15:56:57 +0200

    include jquery as a requirement

--- admin_action_buttons/admin.py
+++ admin_action_buttons/admin.py
@@ -1,9 +1,19 @@
 from django import forms
+from django.conf import settings
+
+
+__all__ = [
+    "ActionButtonsMixin",
+]
 
 
 class ActionButtonsMixin(metaclass=forms.MediaDefiningClass):
+
     class Media:
+        extra = '' if settings.DEBUG else '.min'
         js = [
+            'admin/js/vendor/jquery/jquery%s.js' % extra,
+            'admin/js/jquery.init.js',
             'admin_action_buttons/admin_action_buttons.js',
         ]
         css = {
